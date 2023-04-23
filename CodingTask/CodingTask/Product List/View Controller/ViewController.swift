39ad4de1eb1f30c2de628c6a456226fc3a6fe11d
Suman Chatterjee <suman.chatterjee90@gmail.com>
2023-04-23 12:33:49 +0530

    list view configed with dummy data

--- CodingTask/CodingTask/Product List/View Controller/ViewController.swift
+++ CodingTask/CodingTask/Product List/View Controller/ViewController.swift
@@ -9,12 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    private let productCellReuseId = "ItemsTableViewCell"
+    private let productCellNibName = "ItemsTableViewCell"
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.navigationItem.title = "Products"
+        configTableView()
     }
 
+    private func configTableView(){
+        tableView.separatorStyle = .none
+        tableView.estimatedRowHeight = UITableView.automaticDimension
+        tableView.backgroundColor = .clear
+        tableView.register(UINib(nibName: productCellNibName, bundle: nil), forCellReuseIdentifier: productCellReuseId)
+    }
+}
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: productCellReuseId, for: indexPath) as! ItemsTableViewCell
+        cell.selectionStyle = .none
+        return cell
+    }
 }
 
